1845355f98a5d52743c9b80cafe3fe53a8470ce5
maeva.poirier2022@campus-eni.fr <Axel15042020>
2022-07-19 12:47:02 +0200

    fixtures etats

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -8,40 +8,27 @@ use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
+    private ObjectManager $manager;
+
     public function load(ObjectManager $manager): void
     {
-
-        $ListeEtats = ['En création', 'Ouverte', 'cloturée', 'En cours', 'terminée', 'historisée', 'annulée'];
-
-        for ($listeEtat as $etat) {
-        $etat = new Etat();
-
-        $etat->setLibelle("En création");
-        $manager->persist($etat);
-
-        $etat->setLibelle("Ouverte");
-        $manager->persist($etat);
-
-        $etat->setLibelle("Cloturée");
-        $manager->persist($etat);
-
-        $etat->setLibelle("Activité en cours");
-        $manager->persist($etat);
-
-        $etat->setLibelle("Activité terminée");
-        $manager->persist($etat);
-
-        $etat->setLibelle("Terminée");
-        $manager->persist($etat);
-
-        $etat->setLibelle("Historisée");
-        $manager->persist($etat);
-
-        $etat->setLibelle("Ouverte");
-        $manager->persist($etat);
-
+        $this->manager = $manager;
+        $this->addEtat();
     }
 
-        $manager->flush();
+    public function addEtat(){
+        $listeEtats = ['En création', 'Ouverte', 'cloturée', 'En cours', 'terminée', 'historisée', 'annulée'];
+
+        foreach ($listeEtats as $e) {
+            $etat = new Etat();
+
+            $etat->setLibelle($e);
+            $this->manager->persist($etat);
+
+        }
+
+        $this->manager->flush();
     }
 }
+
+
