f0350d9230a278421bf4bb14e12dcdaf9f62d434
maeva.poirier2022@campus-eni.fr <Axel15042020>
2022-07-26 13:33:43 +0200

    changement automatique des états

--- src/Service/EtatManager.php
+++ src/Service/EtatManager.php
@@ -6,6 +6,8 @@ namespace App\Service;
 
 use App\Entity\Etat;
 use App\Repository\EtatRepository;
+use App\Repository\SortieRepository;
+use DateInterval;
 use Doctrine\ORM\EntityManagerInterface;
 
 class EtatManager
@@ -13,12 +15,14 @@ class EtatManager
 
     private $entityManager;
     private $etatRepository;
+    private $sortieRepository;
 
 
-    public function __construct(EntityManagerInterface $entityManager, EtatRepository $etatRepository)
+    public function __construct(EntityManagerInterface $entityManager, EtatRepository $etatRepository, SortieRepository $sortieRepository)
     {
         $this->entityManager = $entityManager;
         $this->etatRepository = $etatRepository;
+        $this->sortieRepository = $sortieRepository;
     }
 
     public function recupererEtats(String $e): Etat
@@ -34,6 +38,48 @@ class EtatManager
         }
 
         return $monEtat;
+    }
+
+    public function modificationAutomatiqueEtats() : void
+    {
+        $sorties = $this->sortieRepository->findAll();
+        $etatEnCreation = $this->recupererEtats('EN CREATION');
+        $etatOuverte = $this->recupererEtats('OUVERTE');
+        $etatCloturee = $this->recupererEtats('CLOTUREE');
+        $etatAnnulee = $this->recupererEtats('ANNULEE');
+        $etatEnCours = $this->recupererEtats('EN COURS');
+        $etatTerminee = $this->recupererEtats('TERMINEE');
+        $etatHistorisee = $this->recupererEtats('HISTORISEE');
+
+
+
+        foreach ($sorties as $sortie) {
+            $dateHeureFinSortie = $sortie->getDateHeureDebut()->add(new DateInterval( '+ '.$sortie->getDuree().' minutes' ));
+            $dateSortieAHistoriser = $dateHeureFinSortie->add(new DateInterval( '+ 1 month' ));
+
+            //passage etat ouverte -> cloturee
+            if($sortie->getEtat() === $etatOuverte and 'now' > $sortie->getDateLimiteInscription())
+            {
+                $sortie->setEtat($etatCloturee);
+            }
+            //passage etat cloturee -> en cours
+            if(($sortie->getEtat() !== $etatAnnulee or $sortie->getEtat() !== $etatEnCreation) and $sortie->getDateHeureDebut()< 'now' and 'now' < $dateHeureFinSortie)
+            {
+                $sortie->setEtat($etatEnCours);
+            }
+            //passage etat en cours -> terminee
+            if(($sortie->getEtat() !== $etatAnnulee or $sortie->getEtat() !== $etatEnCreation) and $dateHeureFinSortie< 'now' and 'now' < $dateSortieAHistoriser)
+            {
+                $sortie->setEtat($etatTerminee);
+            }
+            //passage etat terminee ou annulee -> historisée
+            if($sortie->getEtat() !== $etatEnCreation and 'now' > $dateSortieAHistoriser)
+            {
+                $sortie->setEtat($etatHistorisee);
+            }
+
+        }
+
     }
 
 
